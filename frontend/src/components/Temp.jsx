93fccc15e6877a402293940d21082549a30ef8ff
Mizanur <jameslev2001@gmail.com>
2023-11-17 13:08:28 +0530

    fix the class name

--- frontend/src/components/Temp.jsx
+++ frontend/src/components/Temp.jsx
@@ -6,9 +6,9 @@ export const Temp = (props) => {
     <div>
 
       <img className="icon" src={link} alt=""></img>
-      <label class="switch">
+      <label className="switch">
         <input type="checkbox" />
-        <span class="slider round"></span>
+        <span className="slider round"></span>
       </label>
     </div>
     <p>{props.name}</p>
