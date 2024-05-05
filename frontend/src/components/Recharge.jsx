3cd884a6327cb645a3f97cdd45866207e3a449f7
Mizanur <jameslev2001@gmail.com>
2024-05-05 18:39:12 +0530

    add:Razorpay frontend

--- frontend/src/components/Recharge.jsx
+++ frontend/src/components/Recharge.jsx
@@ -5,6 +5,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Recharge = () => {
+    const currency = "INR";
+    const receiptId = "qwsaq1";
     const URL = process.env.REACT_APP_BACKEND_URL
     const [card, setCard] = useState()
     const [amount, setAmount] = useState()
@@ -18,10 +20,6 @@ const Recharge = () => {
     }, [])
 
     const funcRecharge = () => {
-        if (amount <= 0) {
-            toast.error("Enter Valid Amount")
-            return;
-        }
         axios.post(`${URL}/card/recharge`, {
             email: localStorage.getItem('email'),
             amount: Number(amount),
@@ -35,7 +33,61 @@ const Recharge = () => {
 
 
 
+    const paymentHandler = async (e) => {
+        if (amount <= 0 || amount=== undefined) {
+            toast.error("Enter Valid Amount")
+            return;
+        }
 
+        const response = await axios.post("https://projectbackend.mizanur.in/api/v1/payment/initiate", {
+            amount: amount * 100,
+            currency,
+            receipt: receiptId,
+        })
+
+        const order = response.data;
+
+        var options = {
+            key: "rzp_test_sB7FZPk7D9C1CB",
+            amount: amount * 100,
+            currency,
+            name: "Quest Card",
+            description: "Test Transaction",
+            image: "https://project.mizanur.in/static/media/login-signup-bg-card.3ff86d8994b9cfd21fba.png",
+            order_id: order.id,
+            handler: async function (response) {
+                const body = {
+                    ...response,
+                };
+
+                const validateRes = await axios.post("https://projectbackend.mizanur.in/api/v1/payment/validate", body);
+
+                const jsonRes = validateRes.data;
+
+                if (jsonRes.msg === 'success') {
+                    funcRecharge()
+                }
+            },
+            prefill: {
+                name: "Quest Card",
+                email: "dev@mizanur.in",
+                contact: "9475149702",
+            },
+            notes: {
+                address: "Razorpay Corporate Office",
+            },
+            theme: {
+                color: "#3399cc",
+            },
+        };
+        var rzp1 = new window.Razorpay(options);
+
+        rzp1.on("payment.failed", function (response) {
+            toast.error("Payment fail try again :(")
+        });
+        rzp1.open();
+        e.preventDefault();
+    }
 
 
     return (
@@ -47,7 +99,7 @@ const Recharge = () => {
                     <div className="body-header">
                         <div>
                             <h2 className="card-heading">Recharge Your Card</h2>
-                            <p className="card-number">{card ? "XXXXXXXXXXXXX"+String(card.cardNumber).substr(-4) : "Loading..."}</p>
+                            <p className="card-number">{card ? "XXXXXXXXXXXXX" + String(card.cardNumber).substr(-4) : "Loading..."}</p>
                             <p className="card-validity">VALILD THRU</p>
                             <p className="card-validity">{card ? card.cardValidity : "Loading..."}</p>
                             <p className="card-about">You Can Recharge your Card</p>
@@ -56,8 +108,9 @@ const Recharge = () => {
                     <div className='transaction-history'>
                         <p className="transaction-history-heading">Recharge </p>
                         <div className="inputCont">
+                            <span>₹</span>
                             <input type="number" autoComplete="off" placeholder='Enter Amount' name='amount' onChange={(e) => { setAmount(e.target.value) }} value={amount} />
-                            <button onClick={funcRecharge}>Recharge</button>
+                            <button onClick={paymentHandler}>Recharge</button>
                         </div>
                     </div>
                 </div>
